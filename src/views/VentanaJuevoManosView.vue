ffb4d26c74329329ffa60f858f94b01853833be1
Cristian Camilo Pineda <62514345+CrisPineda@users.noreply.github.com>
2022-08-20 07:40:17 -0500

    Cambio de letra en el import

--- src/views/VentanaJuevoManosView.vue
+++ src/views/VentanaJuevoManosView.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import JuegoManos from "@/components/Juegos/Manos/juegoManos.vue";
+import JuegoManos from "@/components/Juegos/Manos/JuegoManos.vue";
 </script>
 
 <style lang="scss" scoped>
